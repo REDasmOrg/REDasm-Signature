8f69679ca90ca440022b835e28be774f4a55af9e
Dax <trogu.davide@gmail.com>
2018-12-07 17:36:19 +0100

    Remove unused code

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-#include "psx/psyqlib_generator.h"
 #include "redsigc.h"
 
 int main(int argc, char* argv[])
 {
     REDSigC redsigc(argc, argv);
     return redsigc.run();
-
-    std::string infile = argv[1], outfile = (argc > 2) ? argv[2] : infile;
-
-    PsyQLibGenerator psyqgen;
-
-    if(!psyqgen.saveAsJSON(outfile))
-    {
-        std::cout << "ERROR: Cannot save pattern to " << outfile << std::endl;
-        return 2;
-    }
 }
